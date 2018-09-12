66f5791283524cb07bdda2c8003d321fb839e223
Nico-ehs <nico.ehs05@gmail.com>
2018-09-12 01:07:09 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   r={}
   languages.each_key do |k1|
     k1.each do |k2,v1|
-      r[k2]={}
+      r[k2]={:type => v1, :style => [:functional]}
     end
   end
 end
