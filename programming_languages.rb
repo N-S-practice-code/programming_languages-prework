e8ddda439ebc3e439c8ffc72b810aa270a2c0807
Nico-ehs <nico.ehs05@gmail.com>
2018-09-12 01:13:02 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,11 @@ def reformat_languages(languages)
   r={}
   languages.each_key do |k1|
     languages[k1].each do |k2,v1|
-      if r[k2]
+      if r.has_key(k2)
+        r[k2][:]
+      else
+        
+      end
     end
   end
   r
